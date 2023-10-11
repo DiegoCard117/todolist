554184ede3d5eced82b6e1f9c1147b000cb75632
DiegoCard117 <diegocardoso117@gmail.com>
2023-10-11 10:52:24 -0300

    create tasks

--- src/main/java/todolist/user/UserController.java
+++ src/main/java/todolist/user/UserController.java
@@ -3,6 +3,9 @@ package todolist.user;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +24,13 @@ public class UserController {
     if (user != null) {
       return ResponseEntity.status(400).body("Usuário Ja Cadastrado");
     }
+
+    var passHash = BCrypt
+    .withDefaults()
+    .hashToString(12, userModel.getPassword()
+    .toCharArray());
+
+    userModel.setPassword(passHash);
 
     var useCreated = this.userRepository.save(userModel);
     return ResponseEntity.status(201).body(useCreated);
